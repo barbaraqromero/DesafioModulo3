bdeba82cfd3e365557586d4227fd52b7f3b87f7c
Bárbara Romero <bqromero@gmail.com>
2021-10-08 10:43:42 -0300

    Modelar a classe Pessoa

---
+++ src/br/com/zup/Pessoa.java
@@ -0,0 +1,47 @@
+package br.com.zup;
+
+public abstract class Pessoa {
+  private String nome;
+  private String email;
+  private String cpf;
+
+  public Pessoa(String nome, String email, String cpf) {
+    this.nome = nome;
+    this.email = email;
+    this.cpf = cpf;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder mostrarDados = new StringBuilder();
+    mostrarDados.append("Nome: " + nome);
+    mostrarDados.append("Email: " + email);
+    mostrarDados.append("CPF: " + cpf);
+
+    return mostrarDados.toString();
+  }
+}

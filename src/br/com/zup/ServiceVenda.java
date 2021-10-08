04450ed1a7a709ceef52c99d982ea3f59959fc47
Bárbara Romero <bqromero@gmail.com>
2021-10-08 15:30:41 -0300

    Criar método para pesquisar vendas pelo email do vendedor

--- src/br/com/zup/ServiceVenda.java
+++ src/br/com/zup/ServiceVenda.java
@@ -23,17 +23,30 @@ public class ServiceVenda {
     return vendasCadastradas;
   }
 
-  public static List<Venda> pesquisarVendasPorCpf(String cpf) {
+  public static List<Venda> pesquisarComprasPorCpf(String cpf) {
     List<Venda> comprasPorCpf = new ArrayList<>();
-    for (Venda vendasReferencias : listaDeVendas) {
-      if (vendasReferencias.getCliente().getCpf().equals(cpf)) {
-        comprasPorCpf.add(vendasReferencias);
+    for (Venda comprasReferencias : listaDeVendas) {
+      if (comprasReferencias.getCliente().getCpf().equals(cpf)) {
+        comprasPorCpf.add(comprasReferencias);
       }
     }
+    return comprasPorCpf;
+  }
 
-
-
+  public static List<Venda> pesquisarVendasPorEmail (String email){
+    List<Venda> vendasPorEmail = new ArrayList<>();
+    for (Venda vendasReferencia : listaDeVendas){
+      if (vendasReferencia.getVendedorResponsável().getEmail().equals(email)){
+        vendasPorEmail.add(vendasReferencia);
+      }
+    }
+    return vendasPorEmail;
   }
 }
+
+
+
+
+
 
 

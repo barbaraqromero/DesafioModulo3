7b8b94e76156d9ff9efd873ef38e8caa6d6daf76
Bárbara Romero <bqromero@gmail.com>
2021-10-08 12:41:47 -0300

    Criar método para validar email do cliente

--- src/br/com/zup/ServiceCliente.java
+++ src/br/com/zup/ServiceCliente.java
@@ -6,17 +6,25 @@ import java.util.List;
 public class ServiceCliente {
   private static List<Cliente> listaDeClientes = new ArrayList<>();
 
-  public static Cliente cadastrarCliente (String nome, String email, String cpf, String cartao){
-    Cliente cliente = new Cliente (nome, email,cpf,cartao);
+  public static Cliente cadastrarCliente(String nome, String email, String cpf, String cartao) {
+    Cliente cliente = new Cliente(nome, email, cpf, cartao);
     listaDeClientes.add(cliente);
     return cliente;
   }
 
-  public static List<Cliente> exibirClientes(){
+  public static List<Cliente> exibirClientes() {
     List<Cliente> clientesCadastrados = new ArrayList<>();
-    for(Cliente clienteReferencia : listaDeClientes){
+    for (Cliente clienteReferencia : listaDeClientes) {
       clientesCadastrados.add(clienteReferencia);
     }
     return clientesCadastrados;
+  }
+
+  public static void validarEmailCliente(String email) throws Exception {
+    if (!email.contains("@")) {
+      throw new Exception("E-mail inválido!");
+
+    }
+
   }
 }
